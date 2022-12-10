2fc6166a636330e409a6eaeff32b5457a4eaac65
Kamil Islamov <kam@kambox.ru>
2022-12-10 17:20:54 +0300

    Update 02_dump.sh

--- setup/02_dump.sh
+++ setup/02_dump.sh
@@ -3,4 +3,4 @@ export ORACLE_HOME=/opt/oracle/product/18c/dbhomeXE
 export ORACLE_SID_PDB=pfdb1
 export NLS_LANG=.AL32UTF8
 export ORACLE_SID=XE
-$ORACLE_HOME/bin/impdp kam/kam12345@pfdb1 DUMPFILE=pf_demo.dmp DIRECTORY=DPUMP SCHEMAS="pf"
+$ORACLE_HOME/bin/impdp kam/kam12345@pfdb1 DUMPFILE=pf_trial.dmp DIRECTORY=DPUMP SCHEMAS="pf"
